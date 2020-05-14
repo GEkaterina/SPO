715f1498a0a24ef8f983fed1f73d88620348d910
GEkaterina <31741790+GEkaterina@users.noreply.github.com>
2020-05-14 18:59:13 +0300

    Пример

---
+++ src/exception/TokenException.java
@@ -0,0 +1,18 @@
+package exception;
+
+public class TokenException {
+    public static void main(String[] args) {
+
+        int b = 1;
+        int a = 3;
+
+        while (a < 7) {
+            a = b + a;
+            System.out.println("ОТВЕТ: " + a);
+        }
+
+    }
+
+}
+
+
